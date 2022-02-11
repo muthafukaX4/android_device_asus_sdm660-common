c54cc169bcfcf32df4b57160cd5d2aabd6c1248b
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-02-11 22:55:29 +0530

    sdm660-common: Enable power-libperfmgr
    Change-Id: Ia5f8fb4a2afc7a453eb09d2092a7872fd8442bc9
    Signed-off-by: dotkit <ewprjkt@proton.me>
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -318,8 +318,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.2.vendor \
+    android.hardware.power@1.3-service.asus_sdm660-libperfmgr
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
