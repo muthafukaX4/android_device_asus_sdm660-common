6ef059277e80dd313c7d32de131eca588e071b2a
TheSanty <sudhiryadav.igi@gmail.com>
2023-02-24 10:19:15 +0530

    sdm660-common: Build QTI Perf Vendor Variant
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -325,8 +325,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
-# Power
+# Perf
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
     vendor.qti.hardware.perf@2.2.vendor \
     android.hardware.power.stats@1.0.vendor \
     android.hardware.power-service.asus_sdm660-libperfmgr
