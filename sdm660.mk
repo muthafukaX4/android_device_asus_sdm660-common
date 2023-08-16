fd95abecaa2b55b40e206094b3f8f97c10601350
Kneba <abenkenary3@gmail.com>
2023-08-16 19:02:09 +0000

    sdm660-common: Build LiveDisplay
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -223,6 +223,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.asus_sdm660
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
